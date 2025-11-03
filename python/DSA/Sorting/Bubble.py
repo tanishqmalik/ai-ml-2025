a92c344da4bb810a24d54aa404112194ec8d20d5
tanishqmalik <diamondonlblike2024@gmail.com>
2025-11-03 12:50:28 +0530

    bubble

---
+++ python/DSA/Sorting/Bubble.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]>arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
+    
+    return arr
+
+numbers = [64, 25, 12, 22, 11]
+sorted_numbers = bubble_sort(numbers)
+print(sorted_numbers)
--- python/DSA/Sorting/Selection.py
+++ python/DSA/Sorting/Selection.py
@@ -1,20 +1,38 @@
+# def selection_sort(arr):
+#     n = len(arr)
+
+#     for i in range(n-1):
+#         min_index = i
+
+#         for j in range(i+1,n):
+#             if (arr[j]<arr[min_index]):
+#                 min_index = j
+        
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+
+#     return arr
+
+
+# numbers = [64, 25, 12, 22, 11]
+# sorted_numbers = selection_sort(numbers)
+# print(sorted_numbers)
+
+# "do it on notebook"
+
+
 def selection_sort(arr):
     n = len(arr)
 
     for i in range(n-1):
         min_index = i
 
-        for j in range(i+1,n):
-            if (arr[j]<arr[min_index]):
-                min_index = j
-        
+        for j in range(i+1, n):
+            if(arr[j]<arr[min_index]):
+             min_index=j
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
     return arr
 
-
 numbers = [64, 25, 12, 22, 11]
 sorted_numbers = selection_sort(numbers)
 print(sorted_numbers)
-
-"do it on notebook"
